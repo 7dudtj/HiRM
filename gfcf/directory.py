984ba7ee115e8b935eb59e9f8b6d43a16b31d56a
kgh1030 <kgh1030@yonsei.ac.kr>
2023-04-05 22:10:45 +0900

    skeleton changed

--- gfcf/directory.py
+++
@@ -1,9 +0,0 @@
-from os.path import join
-
-ROOT_PATH = __FILL_YOUT_PATH__
-CODE_PATH = join(ROOT_PATH, 'code')
-DATA_PATH = join(ROOT_PATH, 'data')
-BOARD_PATH = join(CODE_PATH, 'runs')
-FILE_PATH = join(CODE_PATH, 'checkpoints')
-import sys
-sys.path.append(join(CODE_PATH, 'sources'))
